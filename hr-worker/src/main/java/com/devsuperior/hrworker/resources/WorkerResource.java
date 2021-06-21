642b1d49174ea550f496450df6ca286abe5fe7f3
djeymisson <djeymisson@gmail.com>
2021-06-21 17:44:31 -0300

    configuring Zuul timeout

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -36,6 +36,13 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		log.info("port = " + env.getProperty("local.server.port"));
 
 		Worker wk = repository.findById(id).orElse(null);
