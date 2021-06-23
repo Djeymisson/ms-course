357834201386d68d5587fbc358188be864d40914
djeymisson <djeymisson@gmail.com>
2021-06-23 13:29:32 -0300

    login and jwt token configuration

--- hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResource.java
@@ -1,7 +1,5 @@
 package com.devsuperior.hruser.resources;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +16,7 @@ import com.devsuperior.hruser.repositories.UserRepository;
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	private static Logger log = LoggerFactory.getLogger(UserResource.class);
+	// private static Logger log = LoggerFactory.getLogger(UserResource.class);
 
 	@Autowired
 	private UserRepository repository;
